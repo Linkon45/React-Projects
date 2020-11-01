15d769dbba356a67b66d212a48223966f8427f7b
Ali Hasan Md. Linkon <39150694+Linkon45@users.noreply.github.com>
2020-11-01 20:15:26 +0600

    reviews project

---
+++ reviews/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Review from './Review'
+
+function App() {
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>
+            Our Reviews
+          <div className="underline">
+            </div>
+          </h2>
+        </div>
+        <Review />
+      </section>
+    </main>
+
+  );
+}
+
+export default App;
---
+++ reviews/src/Review.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import data from './data'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
+
+
+const Review = () => {
+    const [index, setIndex] = useState(0)
+    const { name, job, image, text } = data[index < 0 ? (index * -1) % 4 : (index % 4)]
+    console.log(index)
+    return (
+        <article className="review">
+            <div className="img-container">
+                <img src={image} alt={name} className="person-img" />
+                <span className="quote-icon">
+                    <FaQuoteRight />
+                </span>
+            </div>
+
+            <h4 className="author">{name}</h4>
+            <p className="job">{job}</p>
+            <p className="info">{text}</p>
+
+            <div className='button-container'>
+                <button onClick={() => setIndex(index - 1)} className="prev-btn">
+                    <FaChevronLeft />
+                </button>
+                <button onClick={() => setIndex(index + 1)} className="next-btn">
+                    <FaChevronRight />
+                </button>
+            </div>
+            <button className="random-btn">
+                Surpise Me
+            </button>
+        </article>
+    )
+}
+
+export default Review
